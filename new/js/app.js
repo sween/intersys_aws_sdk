7d882b924bae369e292beb37e98b780378112af5
sween <ron.sweeney@gmail.com>
2016-03-21 15:20:44 -0400

    Commitasaurus

---
+++ new/js/app.js
@@ -0,0 +1,14 @@
+/**
+* Created with Build Shit Better.
+* User: jacobwaites
+* Date: 2015-08-26
+* Time: 05:44 PM
+* To change this template use Tools | Templates.
+*/
+
+//$(document).ready(function() {
+  //   $('#fixer1').waypoint(function(direction) {
+   // $("#fixer1").css("position",'relative');
+//}
+//});
+    
